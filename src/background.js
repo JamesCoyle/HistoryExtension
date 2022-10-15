71b3fedea978fab489443445d5a0a17a7171a878
James Coyle <github@james-coyle.dev>
2022-10-15 20:14:57 +0100

    Remove logging

--- src/background.js
+++ src/background.js
@@ -26,9 +26,7 @@ function initialize() {
 
 // Updates the action icon to match current theme/icon settings.
 function setIcon() {
-	console.log()
 	chrome.storage.sync.get(['iconColor', 'defaultIconColor']).then(({ iconColor, defaultIconColor }) => {
-		console.log('Icon color set', { iconColor, defaultIconColor })
 		const path = new Path2D('M 8.6 4.5 H 7.4 V 8.6 L 10.1 10.9 L 11 10 L 8.6 8 V 4.5 M 8 1.5 A 6.5 6.5 90 0 0 1.5 8 H 0 L 2.3 10.5 L 4.5 8 H 3 A 5 5 90 0 1 8 3 A 5 5 90 0 1 13 8 A 5 5 90 0 1 8 13 A 5 5 0 0 1 4.3 11.4 L 3.1 12.4 A 6.5 6.5 0 0 0 8 14.5 A 6.5 6.5 90 0 0 14.5 8 A 6.5 6.5 90 0 0 8 1.5')
 		const canvas = new OffscreenCanvas(16, 16)
 		const context = canvas.getContext('2d')
